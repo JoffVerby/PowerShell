482327c89eb0527405c6cb7094707554215403be
JoffVerby <49520617+JoffVerby@users.noreply.github.com>
2019-05-17 15:50:40 +0100

    Create Get-JVPatchFunction.ps1

---
+++ Patching/Get-JVPatchFunction.ps1
@@ -0,0 +1,46 @@
+
+function get-JVPatchList {
+    [CmdletBinding()]
+    param (
+        [string] $TextFilePath
+        
+    )
+    $Servers = Get-Content -Path $TextFilePath
+
+#Action on each server
+foreach ($server in $Servers) {
+
+#Ping Test
+$pingtest = Test-Connection -ComputerName $server -Count 2 -Quiet
+if ($pingtest -eq $false) {
+    $ping = ($server)+" is not Pinging"}
+    else {
+     $ping = ($server)+" Is Pinging"   
+    }
+    $PingResult = $ping
+
+#Get Server Information    
+try {
+    $info = Get-ADComputer -Identity $server -Properties Name,Description,Operatingsystem | Select-Object Name,Description,Operatingsystem
+    
+        $Description =  $info.Description
+        $OS =           $info.Operatingsystem
+    }
+catch {
+        $Description =  "Cannot Find $Server in AD"
+        $OS =           "Cannot Find $Server in AD"
+    }
+
+#Build the table    
+$PatchList = New-Object psobject -Property @{
+
+        Server =        $server
+        Description =   $Description
+        OS  =           $OS
+        PingResult =    $PingResult
+}
+#Output
+$PatchList | Export-Csv C:\Temp_Test\Patchtest.csv -Append -NoTypeInformation
+
+}
+}
